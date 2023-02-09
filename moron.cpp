712b018e183a052212e85b8ee870c9226dad1619
Piyush Bisht <piyushbisht0625@gmail.com>
2023-02-10 01:34:04 +0530

    III

--- moron.cpp
+++ moron.cpp
@@ -1,31 +1,16 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-void moveZeroes(int *arr, int n){
-    int nonZero=0;
-    for(int i=0; i<n; i++){
-        if(arr[i]!=0){
-            swap(arr[i], arr[nonZero++]);
-        }
-    }
-}
-
-void printArr(int *arr, int n){
-    for(int i=0; i<n; i++){
-        cout<<arr[i]<<" ";
-    }
-}
-
 int main(){
-    int arr[100], n;
-    cout<<"Enter the number:- ";
-    cin>>n;
 
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
-    }
+int x=10;
+int y=20;
 
-    moveZeroes(arr, n);
-    printArr(arr, n);
+int temp=x;
+x=y;
+y=temp;
+ 
+ cout<<"The number after swaping" << x << " " <<y  <<endl;
+
 
 }
---
+++ swapping numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int x = 10;
+    int y = 20;
+
+    int temp = x;
+    x = y;
+    y = temp;
+    cout << "The number after swaping" << x << y << endl;
+}
