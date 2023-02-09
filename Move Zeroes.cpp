84b4da6f9514bfd2b69e294df795555b5d856656
Piyush Bisht <piyushbisht0625@gmail.com>
2023-02-10 01:23:16 +0530

    II

---
+++ Move Zeroes.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void moveZeroes(int *arr, int n){
+    int nonZero=0;
+    for(int i=0; i<n; i++){
+        if(arr[i]!=0){
+            swap(arr[i], arr[nonZero++]);
+        }
+    }
+}
+
+void printArr(int *arr, int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int arr[100], n;
+    cout<<"Enter the number:- ";
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    moveZeroes(arr, n);
+    printArr(arr, n);
+
+}
