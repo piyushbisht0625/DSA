dc0870b44cbb11cb556bfa71e3528718cbce97db
Piyush Bisht <piyushbisht0625@gmail.com>
2023-04-08 01:10:54 +0530

    Code

---
+++ PalindromeCheckRE.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(string &str, int start, int end){
+    //base case
+    if(start >= end)
+      return true;
+
+     //Solve on Case
+     if(str[start] != str[end]){
+        return false;
+     }
+
+     //recursion will do rest
+     return isPalindrome(str, start+1, end-1);
+
+}
+
+int main(){
+    cout <<"Enter the string " << endl;
+    string str;
+    cin>>str;
+
+    cout << isPalindrome(str, 0, str.size()-1) << endl;
+
+}
