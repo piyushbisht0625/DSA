3a43bb9d2c52e363ca79ab8cf29e57b306c23ee4
Piyush Bisht <piyushbisht0625@gmail.com>
2023-04-20 00:15:28 +0530

    Code

---
+++ FirstRepeated.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Find first repeated element 
+
+int FirstRepeated(int* arr, int n){
+    unordered_map<int, int >hash;
+    for(int i =0; i<n; i++){
+        hash[arr[i]]++;
+
+    }
+
+    for(int i=0; i<n; i++){
+        if (hash[arr[i]] >1)
+         return i+1;
+    } 
+    return -1;
+}
+
+
+int main(){
+    int arr[]= {1,2,3,4,4,5,6,7,8,4};
+    int n = sizeof(arr)/sizeof(int);
+
+
+    cout<<"The  ans  of thr first repeated index is  " << FirstRepeated(arr,n);
+
+}
---
+++ RotateArray.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void rotateArray(vector<int> &arr, int k){
+
+    int n = arr.size();
+    vector<int> temp(arr.size());
+
+    for(int i =0; i<arr.size(); i++){
+        temp[(i+k)% arr.size()] = arr[i];
+    }
+    //copy temp into arr vector
+    arr = temp;
+}
+
+int main(){
+    vector<int> arr{1,2,3,4,5,6,7};
+    
+    int k;
+    cout << " Enter  value of k:- ";
+    cin>>k;
+    // function call
+    rotateArray(arr, k);
+
+    for(int i=0; i<arr.size(); i++){
+        cout << arr[i] <<" ";
+    }
+    cout << endl;
+
+}
