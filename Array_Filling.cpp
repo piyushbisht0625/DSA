2db58880572766bab62608929bdaafc693592593
Piyush Bisht <piyushbisht0625@gmail.com>
2023-04-26 00:36:59 +0530

    Add files via upload

---
+++ Array_Filling.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n, m;
+        cin >> n >> m;
+
+        vector<pair<int, int>> l;
+
+        for (int i = 0; i < m; i++)
+        {
+            ll x, y;
+            cin >> x >> y;
+            l.push_back({x, y});
+        }
+
+        sort(l.begin(), l.end(), greater<pair<int, int>>());
+
+        ll lcm = 1;
+        ll rem = n;
+        ll res = 0;
+
+        for (int i = 0; i < m && rem > 0; i++)
+        {
+            ll a = l[i].first;
+            ll b = l[i].second;
+
+            lcm = lcm * b / __gcd(lcm, b);
+            res += (rem - n / lcm) * a;
+            rem = n / lcm;
+        }
+
+        cout << res << "\n";
+    }
+    return 0;
+}
