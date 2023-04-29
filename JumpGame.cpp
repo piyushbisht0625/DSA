625e757f110252b29e9762207292d36ef77b6b21
Piyush Bisht <piyushbisht0625@gmail.com>
2023-04-30 00:19:05 +0530

    Add files via upload

---
+++ JumpGame.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool CanJump(vector<int> &arr){
+    int n = arr.size();
+    int rechable = 0;
+
+    for(int i=0; i<n; i++){
+        if(rechable <i)
+        return false;
+
+        rechable = max(rechable, i+arr[i]); // Update the rechable to new 
+    }
+    return true; // rechable>=i condition will return  true;
+
+}
+
+int main(){
+    vector<int> arr{3,2,1,0,4};
+    cout << CanJump(arr)<<endl;
+
+}
