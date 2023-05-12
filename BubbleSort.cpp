79c2bb245b29ff6a0de21d08cd7f7dfbd0ad7742
Piyush Bisht <piyushbisht0625@gmail.com>
2023-05-13 00:07:23 +0530

    Add files via upload

---
+++ BubbleSort.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    vector<int> arr{11,13,15,78,2,4,100,67,56,59,10,1,7,6,14,9,90,23,45};
+    int n = arr.size();
+
+    for(int round =1; round <n-1; round++){
+        
+        for(int j=0; j< n-round; j++){ 
+
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+
+    //Printing
+    for( int i = 0; i<n; i++){
+        cout << arr[i] <<" ";
+
+    }
+    cout << endl;
+
+
+}
