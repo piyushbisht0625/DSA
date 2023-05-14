2a2ba029459efbfa60f1bcab8cd5989d7bbb3451
Piyush Bisht <piyushbisht0625@gmail.com>
2023-05-14 12:59:02 +0530

    Add files via upload

---
+++ ConvertToUpperrCase.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using  namespace std;
+
+void Uppercase( char arr[]){
+
+    int n = strlen(arr);
+    for (int i = 0; i<n; i++){
+        arr[i] =arr[i]-'a' + 'A';
+    }
+}
+
+
+
+
+int main(){
+
+char arr[100];
+cin.getline(arr, 100);
+Uppercase(arr);
+
+cout << arr << endl;
+
+ 
+return 0;
+
+
+}
+
