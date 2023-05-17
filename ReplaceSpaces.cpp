5bcf6befe22adb787dae89bfd9ce4ccbc6593d4b
Piyush Bisht <piyushbisht0625@gmail.com>
2023-05-17 14:05:31 +0530

    Add files via upload

---
+++ ReplaceSpaces.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#include<string.h>
+using  namespace std;
+
+
+void replaceSpaces( char sentence[ ]){
+
+int i = 0;
+int n = strlen(sentence);
+
+for( int i =0; i<n; i++){
+    if(sentence[i]== ' '){
+        sentence[i] = '@';
+    }
+}
+}
+
+
+
+int main(){
+
+char sentence[100];
+cin.getline(sentence, 100);
+
+replaceSpaces(sentence);
+cout << "printing sentence" << endl << sentence << endl;
+
+return 0;
+
+
+}
+
