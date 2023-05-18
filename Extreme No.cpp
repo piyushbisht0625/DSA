a95e7cc86c34c2a0b1d229149704f4af01dd6f39
Piyush Bisht <piyushbisht0625@gmail.com>
2023-05-18 20:45:15 +0530

    Add files via upload

---
+++ Extreme No.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[]= {10,20,30,40,50,60,70,80,90};
+    int size =9;
+    
+
+    int start=0;
+    int end= size -1;
+
+    while(start<=end){
+
+        if (start== end){
+            cout << arr[start] <<" ";
+        }
+        else{
+            cout << arr[start]<<" ";
+            cout << arr[end] <<" ";
+        }
+        start++;
+        end--;
+        
+    }
+
+
+}
+
