b38b640e5953b85c35b7543591168b5ea7b8fff2
Piyush Bisht <piyushbisht0625@gmail.com>
2023-05-22 00:52:01 +0530

    Add files via upload

---
+++ Olympic_Ranking.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        
+        int g1,s1,b1,g2,s2,b2;
+        cin>>g1>>s1>>b1>>g2>>s2>>b2;
+
+        int country1=g1+s1+b1;
+        int country2=g2+s2+b2;
+
+        if(country1 > country2){
+            cout<<1;
+        }
+        else{
+            cout<<2;
+        }
+    }
+    return 0;
+}
