4f3dbf95d06a05c66dd6343be7ee67224158f472
Piyush Bisht <piyushbisht0625@gmail.com>
2023-05-23 00:42:23 +0530

    Add files via upload

---
+++ problem_difficulties.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+
+    while(t--){
+        int a1,a2,a3,a4;
+        cin>>a1>>a2>>a3>>a4;
+
+        if(a1==a2&& a2==a3 && a3==a4){
+            cout<<0<<"\n";
+        }
+        else if((a1==a2 && a2==a3) || (a2==a3 && a3==a4) || (a3==a4 && a4==a1) || (a4==a1 &&a1==a2)){
+            cout<<1<<"\n";
+        }
+        else{
+            cout<<2<<"\n";
+        }
+    }
+    return 0;
+}
