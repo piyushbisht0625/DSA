d1aa07a4e67f7e6983577e477160948e4e92b4a4
Piyush Bisht <piyushbisht0625@gmail.com>
2023-05-26 00:49:16 +0530

    Add files via upload

---
+++ pattern1.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int main( ){
+    int n;
+    cin>>n;
+      int row=1;
+
+
+    while( row<n){
+
+        int col=1;
+        while(col<n){
+            cout<<"#"<<" ";
+            col++;
+        }
+        cout<<endl;
+         row++;
+    }
+
+
+
+
+
+    
+ }
