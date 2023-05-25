4f12e7c381bf7ee721a8d5991ffb23ad3174e04a
Piyush Bisht <piyushbisht0625@gmail.com>
2023-05-26 00:52:41 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main(){
 
-    vector<int> arr{11,13,15,78,2,4,100,67,56,59,10,1,7,6,14,9,90,23,45};
-    int n = arr.size();
+    vector<int> arr{11,13,15,78,2,4,100,67,56,59,10,1,7,6,14,9,90,23,45};// input array
+    int n = arr.size(); // calculate size
 
     for(int round =1; round <n-1; round++){
         
         for(int j=0; j< n-round; j++){ 
 
             if(arr[j] > arr[j+1]){
-                swap(arr[j], arr[j+1]);
+                swap(arr[j], arr[j+1]); // swaping j with j+1
             }
         }
     }
