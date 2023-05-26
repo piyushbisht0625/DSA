1f625b01a9457cf2994f9710eae98a0b93bab60d
Piyush Bisht <piyushbisht0625@gmail.com>
2023-05-27 01:55:39 +0530

    Add files via upload

---
+++ pattern2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int i=1;           
+    
+    
+
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<i<<" ";     /* 1 1 1
+                                2  2  2
+                                3  3  3
+                */
+            j++;
+        }
+        i++;
+        cout<<endl;
+
+    }
+
+
+
+}
+
