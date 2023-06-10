f5996a29f7a3a05f1c9fde78a6c9ffa31a29001e
Piyush Bisht <piyushbisht0625@gmail.com>
2023-06-10 21:59:17 +0530

    Add files via upload

---
+++ duplicate_numbers.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void duplicate(int arr[],int n){
+    int check[100]={};
+    for(int i=0;i<n;i++){
+        check[arr[i]]++;
+    }
+    for(int i=0;i<n;i++){
+        if(check[i] >= 2){
+            cout<<"Duplicate is"<<" "<<i<<endl;
+        }
+        if(check[i]==0){
+            cout<<"Missing is"<<" "<<i<<endl;
+        }
+    }
+}
+int main(){
+    int n;
+    cout<<"enter the size of array"<<" ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+          cin>>arr[i];
+    }
+    duplicate(arr,n);
+    return 0;
+}
