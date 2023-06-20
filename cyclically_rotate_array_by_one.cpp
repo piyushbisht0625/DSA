5d0a28757f60ef8062b6d29949e60f7331a0119a
Piyush Bisht <piyushbisht0625@gmail.com>
2023-06-20 23:59:13 +0530

    Add files via upload

---
+++ cyclically_rotate_array_by_one.cpp
@@ -0,0 +1,22 @@
+//k left-right rotation
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the size of array"<<" ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+
+    }
+    int temp=arr[0];
+    for(int i=0;i<n;i++){
+        arr[i]=arr[i+1];
+    }
+    arr[n-1]=temp;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
