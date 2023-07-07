e8b08cc00d54a7bb307d9380be7b6a94673f6019
Piyush Bisht <piyushbisht0625@gmail.com>
2023-07-07 11:24:07 +0530

    Add files via upload

---
+++ kth_smallest_and_largest.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+priority_queue<int>maxh;
+int kthsmallest(int arr[],int n,int k){
+    for(int i=0;i<n;i++){
+        maxh.push(arr[i]);
+        if(maxh.size()>k){
+            maxh.pop();
+        }
+    }
+    return maxh.top();
+}
+int main(){
+     int n;
+    cout<<"enter the size"<<" ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int k;
+    cout<<"enter the value of k"<<" ";
+    cin>>k;
+    cout<<kthsmallest(arr,n,k);
+    return 0;
+}
