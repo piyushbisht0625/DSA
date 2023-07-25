a8c681fccfa685d70dadc984c81ab052539508fa
Piyush Bisht <piyushbisht0625@gmail.com>
2023-07-25 12:55:26 +0530

    swap .cpp

--- CODE.cpp
+++ CODE.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main (){
 
 int x=10;
 int y=20;
 
 int temp=x;
-x=y;
-y=temp;
+x = y;
+y = temp;
  
  cout<<"The number after swaping" << x << " " <<y  <<endl;
 
