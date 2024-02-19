00cb5ec171ba1c32b0cad027cb1d801f6319b976
Piyush Bisht <piyushbisht0625@gmail.com>
2024-02-20 01:14:37 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -8,7 +8,7 @@ int main(){
        
      vector<int> arr {11,13,15,78,2,4,100,67,56,59,10,1,7,6,14,9,90,23,45};   // input array
     int n = arr.size(); // calculate size
-
+ 
     for(int round =1; round <n-1; round++){
         
         for(int j=0; j< n-round; j++){ 
