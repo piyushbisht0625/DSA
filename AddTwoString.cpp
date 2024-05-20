b54b047c738a6e8d8110bc618563f2f83dfa48d3
Piyush Bisht <piyushbisht0625@gmail.com>
2024-05-20 21:45:22 +0530

    Update AddTwoString.cpp

--- AddTwoString.cpp
+++ AddTwoString.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
  
  
-    void addRE(string&num1, int p1, string&num2, int p2, int carry, sring &ans){
+    void addRE(string&num1, int p1, string&num2, int p2, int carry, sring &ans){ 
         //Base case   
         if(p1 < 0 && p2 < 0){
             if(carry !=0){
